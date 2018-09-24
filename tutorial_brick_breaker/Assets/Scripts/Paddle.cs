0e9655b3f4ade81f010079ae3266f1df744418b9
Manny <mvilla1990@gmail.com>
2018-09-24 00:39:56 -0500

    Added game over and level 1 scenes
    Buttons are now fully hooked up from start menu to level 1 and game over to quit or start menu.

---
+++ tutorial_brick_breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+  [SerializeField] float screenWidthInUnits = 16f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    var paddleX = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+    paddleX = Mathf.Clamp(paddleX, 1f, 15f);
+    transform.position = new Vector2(paddleX, transform.position.y);
+	}
+}
