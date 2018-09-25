8e71d4ddd103baad16cb4eb68b9c1ee16c49ea7b
Manny <mvilla1990@gmail.com>
2018-09-24 23:20:47 -0500

    Added sound
    Sounds for ball bounces on collision

--- tutorial_brick_breaker/Assets/Scripts/Ball.cs
+++ tutorial_brick_breaker/Assets/Scripts/Ball.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,16 +7,20 @@ public class Ball : MonoBehaviour {
   [SerializeField] Paddle paddle;
   [SerializeField] float xPush = 1f;
   [SerializeField] float yPush = 15f;
+  [SerializeField] List<AudioClip> audioClips;
+
+  AudioSource audioSource;
   Vector3 ballOffset;
   bool hasGameStarted = false;
-  Rigidbody2D rigidbody2D;
+  Rigidbody2D rigBody;
 
   private void Start()
   {
-    rigidbody2D = GetComponent<Rigidbody2D>();
-    ballOffset = transform.position - paddle.transform.position;
+    rigBody = GetComponent<Rigidbody2D>();
+    audioSource = GetComponent<AudioSource>();
 
-    rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+    ballOffset = transform.position - paddle.transform.position;
+    rigBody.bodyType = RigidbodyType2D.Kinematic;
   }
 
   // Update is called once per frame
@@ -31,12 +34,18 @@ public class Ball : MonoBehaviour {
     LockBallToPaddle();
 	}
 
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    audioSource.clip = GetRandomBallHitClip();
+    audioSource.Play();
+  }
+
   private void LaunchOnMouseClick()
   {
     if (Input.GetMouseButtonDown(1))
     {
-      rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
-      rigidbody2D.velocity = new Vector2(xPush, yPush);
+      rigBody.bodyType = RigidbodyType2D.Dynamic;
+      rigBody.velocity = new Vector2(xPush, yPush);
       hasGameStarted = true;
     }
   }
@@ -44,5 +53,10 @@ public class Ball : MonoBehaviour {
   void LockBallToPaddle()
   {
     transform.position = new Vector3(paddle.transform.position.x, transform.position.y);
+  }
+
+  AudioClip GetRandomBallHitClip()
+  {
+    return audioClips[Random.Range(0, audioClips.Count)];
   }
 }
--- tutorial_brick_breaker/Assets/Scripts/Brick.cs
+++ tutorial_brick_breaker/Assets/Scripts/Brick.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
+  [SerializeField] AudioClip onDestroySoundClip;
+
   private void OnCollisionEnter2D(Collision2D collision)
   {
     Destroy(gameObject);
+    AudioSource.PlayClipAtPoint(onDestroySoundClip, new Vector3());
   }
 }
